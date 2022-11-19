5a3b5eed17f8fe524a0216bd86b91fa3c1a18e00
NyxHexen <jd.davis948@gmail.com>
2022-11-19 18:07:37 +0000

    Refactored See All/Hide switch function on user profile; added conditional to check screen size and remote text in card-action

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.sidenav').sidenav();
 });
 
-$('.profile-languages .card-action a').click(function() {
+$('.profile-languages .card-action a').click(function () {
   $('.user-languages').toggleClass('truncate');
-  if (this.text == "See All") {
-    this.text = "Hide"
+  if ($(this).text() == "See All") {
+    let text = $(this).text();
+    $(this).text(text.replace("See All", "Hide"))
   } else {
-    this.text = "See All"
+    let text = $(this).text();
+    $(this).text(text.replace("Hide", "See All"))
   }
 });
+
+// $(window).innerWidth() returns 17px less as a value
+if ($(window).innerWidth() + 17 <= 740) {
+  $('.post .card-action span').each(function () {
+    let text = $(this).text();
+    text = text.replace("comments", "");
+    $(this).text(text)
+  });
+}
