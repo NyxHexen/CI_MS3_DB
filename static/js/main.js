898c7cc8dac2ccb60e72bd728bea36eef142ce82
NyxHexen <jd.davis948@gmail.com>
2022-11-19 12:09:43 +0000

    Toggle between text on show all  button for user-languages

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$('.profile-languages .card-action a').click(function() {
+  $('.user-languages').toggleClass('truncate');
+  if (this.text == "See All") {
+    this.text = "Hide"
+  } else {
+    this.text = "See All"
+  }
+});
