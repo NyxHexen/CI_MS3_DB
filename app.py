75fc1310a7c89a48687fba21725dd54fa7ff44b2
NyxHexen <jd.davis948@gmail.com>
2022-11-20 12:54:57 +0000

    Added form validation flash messages logic

--- app.py
+++ app.py
@@ -31,9 +31,15 @@ def signup():
         return redirect(url_for('signin'))
     return render_template("signup.html", title="Sign Up", form=form)
 
-@app.route("/signin")
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.id.data == 'admin@devbus.com' and form.password.data == 'password':
+            flash("You have been logged in!", 'light-blue lighten-2')
+            return redirect(url_for('home'))
+        else: 
+            flash('Login Unsuccessful. Please check your username (or email) and password.', 'materialize-red lighten-1')
     return render_template("signin.html", title="License and Registration", form=form)
 
 
