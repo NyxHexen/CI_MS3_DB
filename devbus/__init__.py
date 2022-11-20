1720e13d9d4d46eb53383ab65fc9b7b2e26e5528
NyxHexen <jd.davis948@gmail.com>
2022-11-20 20:43:43 +0000

    Removed PyMongo import; setup default login_view for login_manager

--- devbus/__init__.py
+++ devbus/__init__.py
@@ -1,7 +1,6 @@
 import os
 from bson.objectid import ObjectId
 from flask import Flask
-from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_mongoengine import MongoEngine
 from flask_login import LoginManager
@@ -18,5 +17,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 db = MongoEngine(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
 
 from devbus import routes
