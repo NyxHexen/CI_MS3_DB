a51508815ba7af7712256f1a5fa19fc54b692096
NyxHexen <jd.davis948@gmail.com>
2022-11-22 23:39:18 +0000

    Added form and validation to allow account detail updates

--- devbus/routes.py
+++ devbus/routes.py
@@ -3,7 +3,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_login import login_user, current_user, logout_user, login_required
 from devbus import app, bcrypt
-from devbus.forms import RegistrationForm, LoginForm
+from devbus.forms import SignUpForm, SignInForm, UpdateProfileForm
 from devbus.models import User, Post
 from devbus.utils import client
 
@@ -18,7 +18,7 @@ def home():
 def signup():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    form = RegistrationForm()
+    form = SignUpForm()
     if form.validate_on_submit():
         user = User(username = form.username.data,
                     password = bcrypt.generate_password_hash(form.password.data).decode('utf-8'),
@@ -35,7 +35,7 @@ def signup():
 def signin():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    form = LoginForm()
+    form = SignInForm()
     if form.validate_on_submit():
         user = User.objects(email=form.email.data).first()
         print(user)
@@ -55,7 +55,17 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/profile")
+@app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
-    return render_template("profile.html", title="Profile")
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.f_name = form.f_name.data
+        current_user.l_name = form.l_name.data
+        current_user.languages = form.languages.data
+        current_user.bio = form.bio.data
+        current_user.save()
+        flash('Got it! Your profile has been updated.', 'message')
+    return render_template("profile.html", title="Profile", form=form)
