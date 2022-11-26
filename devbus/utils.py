be98be8587f10c91e896da4b1ffe4f319cf80de9
NyxHexen <jd.davis948@gmail.com>
2022-11-26 20:08:24 +0000

    Created helper function to crop and resize images from user before uploading to AWS S3 bucket

--- devbus/utils.py
+++ devbus/utils.py
@@ -1,5 +1,6 @@
-import boto3, os, secrets
+import boto3, os, secrets, io
 from botocore.exceptions import ClientError
+from PIL import Image, ImageDraw
 
 # AWS S3 variables
 s3_bucket_name = "ci-ms3-devbus"
@@ -17,18 +18,49 @@ def upload_image(file):
     tries to upload it to the bucket. If successful, returns
     the link to the image to be stored in DB.
     """
+
+    image = resize_image(file)
+
+    # Generate random hex value
     random_hex = secrets.token_hex(8)
     # Using _ as throwaway variable
     _, file_ext = os.path.splitext(file.filename)
+    # New image name using random hex value and
+    # extension of file.
     image_name = random_hex + file_ext
 
     try:
         s3 = boto3.resource('s3')
         s3.Bucket(s3_bucket_name).put_object(
                 Key=image_name,
-                Body=file)
+                Body=image)
     except ClientError:
         raise Exception("There was a problem uploading the image to the AWS S3 bucket")
     
     image_url = s3_bucket_url + image_name
     return image_url
+
+
+def resize_image(file):
+    # Open Image
+    image = Image.open(file)
+    image_w, image_h = image.size
+
+    if image_w > image_h:
+        image_diff = (image_w - image_h) / 2
+        cropped_image = image.crop((image_diff, 0, image_w - image_diff , image_h))
+    elif image_h > image_w:
+        image_diff = (image_h - image_w) / 2
+        cropped_image = image.crop((0, image_diff, image_w, image_h - image_diff))
+
+    # Set size tuple to which image should be resized to
+    # Resize the image to dimensions from size var
+    (cropped_image if cropped_image else image).thumbnail((350, 350), Image.Resampling.LANCZOS)
+    # Save the image to an in-memory file
+    to_memory = io.BytesIO()
+    (cropped_image if cropped_image else image).save(to_memory, format=image.format)
+    to_memory.seek(0)
+
+    return to_memory
+
+
