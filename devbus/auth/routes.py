34ae9a3a4bda16cd45f248d364ef4b5f34e3c9d0
NyxHexen <jd.davis948@gmail.com>
2022-11-29 20:02:01 +0000

    CHANGE: redirect links now use path instead of url_for; implemented temporary function to test gen of url from token; finished reset_password route for now

--- devbus/auth/routes.py
+++ devbus/auth/routes.py
@@ -3,7 +3,6 @@ from flask import (
     flash, render_template,
     redirect, request, url_for, Blueprint)
 from flask_login import login_user, current_user, logout_user, login_required
-from itsdangerous import TimedSerializer
 from devbus import bcrypt, app
 from devbus.auth.forms import (
     SignUpForm, SignInForm, UpdateProfileForm, ForgotPwdForm, NewPwdForm)
@@ -16,7 +15,7 @@ auth = Blueprint('auth', '__name__')
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('main.home'))
+        return redirect("/")
     form = SignUpForm()
     if form.validate_on_submit():
         user = User(username = form.username.data,
@@ -33,7 +32,7 @@ def signup():
 @auth.route("/signin", methods=["GET", "POST"])
 def signin():
     if current_user.is_authenticated:
-        return redirect(url_for('main.home'))
+        return redirect("/")
     form = SignInForm()
     if form.validate_on_submit():
         user = User.objects(email=form.email.data).first()
@@ -41,7 +40,7 @@ def signin():
             login_user(user, remember=form.remember.data)
             next = request.args.get('next')
             flash(f"Hi, {user.f_name}!" if user.f_name else f"Welcome, {user.username}!")
-            return redirect(next) if next else redirect(url_for('main.home'))
+            return redirect(next) if next else redirect("/")
         else:
             flash('Login Unsuccessful. Please check your email or password.', 'materialize-red lighten-1')
     return render_template("signin.html", title="License and Registration", form=form)
@@ -50,7 +49,7 @@ def signin():
 @auth.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('main.home'))
+    return redirect("/")
 
 
 @auth.route("/profile")
@@ -85,20 +84,36 @@ def edit_profile():
 @auth.route("/forgot_password", methods=["GET", "POST"])
 def forgot_password():
     if current_user.is_authenticated: 
-        return redirect("home")
+        return redirect("/")
     form = ForgotPwdForm()
     if form.validate_on_submit():
         user = User.objects.get(email=form.email.data)
         if user is not None:
             token = user.generate_pwd_token()
-            return render_template("forgot_password.html", title="Forgotten Password?", form=form, token=token) # delete before pushing
+            form.email.data = url_for('auth.reset_password', token=token, _external=True)
+            return render_template("forgot_password.html", title="Forgotten Password?", form=form, token=token) # currently loads the token link in the email field
             # url_for('auth.reset_password', token=token, _external=True) print the link to the token
     return render_template("forgot_password.html", title="Forgotten Password?", form=form)
 
 
-@auth.route("/reset_password/<token>")
+@auth.route("/reset_password/<token>", methods=["GET", "POST"])
 def reset_password(token):
+    if current_user.is_authenticated:
+        flash("You are already logged in!", "yellow black-text")
+        return redirect("/")
+
     form = NewPwdForm()
-    if current_user.is_authenticated: 
-        return redirect("home")
+
+    try:
+        user_id = User.verify_pwd_token(token)
+    except:
+        flash("This token is no longer valid. Please try again!", "materialize-red")
+        return redirect("/signin")
+
+    user = User.objects.get(id=user_id)
+    if form.validate_on_submit():
+        user.password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.save()
+        flash("Your password has been reset! You can now login.", "green")
+        return redirect("/signin")
     return render_template("reset_password.html", title="Reset Password", form=form)
