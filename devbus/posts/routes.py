a971339224e6502117d329c234f5b5b5666f66e8
NyxHexen <jd.davis948@gmail.com>
2022-11-29 21:26:33 +0000

    ADD: created view single post route

--- devbus/posts/routes.py
+++ devbus/posts/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from devbus.utils.models import Post, Comment, User
 
 
 posts = Blueprint('posts', '__name__')
+
+
+@posts.route("/post/<id>")
+def view_post(id):
+    post = Post.objects.get(id=id)
+    return render_template("view_post.html", post=post)
