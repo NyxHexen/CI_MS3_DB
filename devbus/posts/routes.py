3f6a9e7a61e178d504d612409b202de7a2da579e
NyxHexen <jd.davis948@gmail.com>
2022-12-01 22:03:12 +0000

    ADD: new route for creating new posts

--- devbus/posts/routes.py
+++ devbus/posts/routes.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, render_template
-from devbus.utils.models import Post, Comment, User
+from flask_login import login_required
+from devbus.utils.models import Post, Comment
+from devbus.posts.forms import NewPostForm
 
 
 posts = Blueprint('posts', '__name__')
 
 
 @posts.route("/post/<id>")
+@login_required
 def view_post(id):
     post = Post.objects.get(id=id)
     return render_template("view_post.html", post=post)
+
+
+@posts.route("/new_post")
+@login_required
+def new_post():
+    form = NewPostForm()
+    return render_template("new_post.html", form=form)
