c0bd1ee6d6629e71ed7652d139244faa61f26a88
NyxHexen <jd.davis948@gmail.com>
2022-12-03 13:30:18 +0000

    ADD: logic to push new posts and new comments to MongoDB

--- devbus/posts/routes.py
+++ devbus/posts/routes.py
@@ -1,21 +1,33 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required, current_user
 from devbus.utils.models import Post, Comment
-from devbus.posts.forms import NewPostForm
+from devbus.posts.forms import NewPostForm, NewCommentForm
 
 
 posts = Blueprint('posts', '__name__')
 
 
-@posts.route("/post/<id>")
+@posts.route("/post/<id>", methods=["GET", "POST"])
 @login_required
 def view_post(id):
     post = Post.objects.get(id=id)
-    return render_template("view_post.html", post=post)
+    form = NewCommentForm()
+    if form.validate_on_submit():
+        comment = Comment()
+        form.populate_obj(comment)
+        comment.save()
+        return redirect(f"/post/{id}")
+    return render_template("view_post.html", post=post, form=form)
 
 
-@posts.route("/new_post")
+@posts.route("/new_post", methods=["GET", "POST"])
 @login_required
 def new_post():
     form = NewPostForm()
+    if form.validate_on_submit():
+        post = Post()
+        form.populate_obj(post)
+        post.created_by = current_user.id
+        post.save()
+        return redirect(f"/post/{post.id}")
     return render_template("new_post.html", form=form)
