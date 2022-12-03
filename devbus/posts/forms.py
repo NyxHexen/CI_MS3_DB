e3e9661d5a84440136a0aae61b69edbdbf0a1591
NyxHexen <jd.davis948@gmail.com>
2022-12-03 13:29:29 +0000

    ADD: form teplate for new comments

--- devbus/posts/forms.py
+++ devbus/posts/forms.py
@@ -8,10 +8,17 @@ from wtforms.validators import DataRequired, Length
 class NewPostForm(FlaskForm):
     post_title = StringField('Post Title',
                                     validators=[DataRequired()])
-    post_content = StringField('Post Content', 
+    post_content = TextAreaField('Post Content', 
                                     validators=[DataRequired()])
     code_content = TextAreaField('Code Content', 
                                     validators=[])
     code_language = StringField('Code Language')
     post_type = RadioField("Post Type", choices=["post", "assist"],default="post")
+    submit = SubmitField('POST')
+
+
+class NewCommentForm(FlaskForm):
+    comment_content = TextAreaField()
+    code_language = StringField()
+    code_content = TextAreaField()
     submit = SubmitField('POST')
