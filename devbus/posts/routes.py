687057a83e56977a3fe3321b4af65c649a68b10d
NyxHexen <jd.davis948@gmail.com>
2022-12-04 15:05:29 +0000

    ADD: new route which gets called by an AJAX function; route checks if current user has voted and updates accordingly; returns response methods

--- devbus/posts/routes.py
+++ devbus/posts/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request, jsonify, flash
 from flask_login import login_required, current_user
 from devbus.utils.models import Post, Comment
 from devbus.posts.forms import NewPostForm, NewCommentForm
@@ -40,3 +40,30 @@ def new_post():
         post.save()
         return redirect(f"/post/{post.id}")
     return render_template("new_post.html", form=form)
+
+
+@posts.route("/_update_votes/<id>/<vote>", methods=["GET", "POST"])
+def update_votes(id, vote):
+    if current_user.is_authenticated is False:
+        flash("You must be signed in to do that!", "red")
+        return jsonify(False)
+    content = Post.objects(id=id).first()
+    if current_user in content.votes["up"] and vote == "up":
+        # If the user has upvoted before, and is pressing upvote btn again
+        content.votes["up"].remove(current_user)
+    elif current_user in content.votes["up"] and vote == "down":
+        # If the user has upvoted before, and is pressing downvote btn
+        content.votes["up"].remove(current_user)
+        content.votes["down"].append(current_user)
+    elif current_user in content.votes["down"] and vote == "up":
+        # If the user has downvoted before, and is pressing upvote btn
+        content.votes["down"].remove(current_user)
+        content.votes["up"].append(current_user)
+    elif current_user in content.votes["down"] and vote == "down":
+        # If the user has downvoted before, and is pressing downvote btn again
+        content.votes["down"].remove(current_user)
+    else:
+        # If the user has never voted before
+        content.votes[vote].append(current_user)
+    content.save()
+    return jsonify(length_up = len(content.votes["up"]), length_down = len(content.votes["down"]))
