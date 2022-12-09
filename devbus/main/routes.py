2e18d37dd3f6345b3f24b1a50ecab92fcbf403e0
NyxHexen <jd.davis948@gmail.com>
2022-12-09 19:40:01 +0000

    ADD: route for view_user route

--- devbus/main/routes.py
+++ devbus/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,Blueprint
-from devbus.utils.models import Post
+from devbus.utils.models import Post, User
 
 main = Blueprint("main", "__name__")
 
@@ -7,3 +7,9 @@ main = Blueprint("main", "__name__")
 def home():
     posts = Post.objects()
     return render_template("home.html",posts=posts)
+
+@main.route("/<username>")
+def view_user(username):
+    user = User.objects.get(username=username)
+    posts = Post.objects(created_by=user)
+    return render_template("view_user.html", user=user, posts=posts)
